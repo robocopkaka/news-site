e98c52fefda06bd0ed6e730db9d935c05ef777ac
Okereke Onyekachi <onyekachi.o@ng.lopworks.com>
2017-05-23 17:17:21 +0100

    Started making it possible to collect the results of different api calls for different sources

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,17 @@
 class UsersController < ApplicationController
 	require 'json'
 	def index
-		response = HTTParty.get "https://newsapi.org/v1/articles?source=cnn&sortBy=top&apiKey=7fb58d3b13714489ac9362f4f9cd5f9c"
-		@articles = JSON.parse(response.body)["articles"]
+		Rails.cache.fetch("recent_articles", expires_in: 12.hours) do
+		  User.first.sources.each_with_index do |source|
+		  	response = HTTParty.get "https://newsapi.org/v1/articles?source=#{source}&sortBy=top&apiKey=7fb58d3b13714489ac9362f4f9cd5f9c"
+		  	# this should generate variable names like cnn_articles, reuters_articles
+		  	"#{source}_articles".to_sym = JSON.parse(response.body)["articles"]
+		  	@articles << "articles_#{index}".to_sym
+		  end
+		end
 	end
 
-	def add_source
-
+	def add_source(*options)
+		
 	end
 end
